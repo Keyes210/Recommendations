abeda95aead00d558635f490eb2a6f53c603dd0a
Alex Lowe <loweab@gmail.com>
2015-07-24 03:30:51 -0400

    course complete, but app crashes in several instances

--- app/src/main/java/com/alexlowe/recommendations/ListingAdapter.java
+++ app/src/main/java/com/alexlowe/recommendations/ListingAdapter.java
@@ -1,16 +1,26 @@
 package com.alexlowe.recommendations;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.alexlowe.recommendations.api.Etsy;
+import com.alexlowe.recommendations.google.GoogleServicesHelper;
 import com.alexlowe.recommendations.model.ActiveListings;
 import com.alexlowe.recommendations.model.Listing;
+import com.google.android.gms.plus.PlusOneButton;
+import com.google.android.gms.plus.PlusShare;
 import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -20,15 +30,20 @@ import retrofit.client.Response;
  * Created by Alex on 7/23/2015.
  */
 public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingHolder>
-    implements Callback<ActiveListings>{
+    implements Callback<ActiveListings>, GoogleServicesHelper.GoogleServicesListener{
+
+    public static final int REQUEST_CODE_PLUS_ONE = 10;
+    public static final int REQUEST_CODE_SHARE = 11;
 
     private MainActivity activity;
     private LayoutInflater inflater;
     private ActiveListings activeListings;
+    private boolean isPlayAvailable;
 
     public ListingAdapter(MainActivity activity){
         this.activity = activity;
         inflater = LayoutInflater.from(activity);
+        this.isPlayAvailable = false;
     }
 
     @Override
@@ -51,6 +66,46 @@ public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingH
         listingHolder.shopNameView.setText(listing.Shop.shop_name);
         listingHolder.priceView.setText(listing.price);
 
+        listingHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openListing = new Intent(Intent.ACTION_VIEW);
+                openListing.setData(Uri.parse(listing.url));
+                activity.startActivity(openListing);
+            }
+        });
+
+        if(isPlayAvailable){
+            listingHolder.plusOneButton.setVisibility(View.VISIBLE);
+            listingHolder.plusOneButton.initialize(listing.url, REQUEST_CODE_PLUS_ONE);
+            listingHolder.plusOneButton.setAnnotation(PlusOneButton.ANNOTATION_NONE);
+
+            listingHolder.shareButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new PlusShare.Builder(activity)
+                            .setType("text/plain")
+                            .setText("Checkout this item on Etsy " + listing.title)
+                            .setContentUrl(Uri.parse(listing.url))
+                            .getIntent();
+                    activity.startActivityForResult(intent, REQUEST_CODE_SHARE);
+                }
+            });
+        }else{
+            listingHolder.plusOneButton.setVisibility(View.GONE);
+
+            listingHolder.shareButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.putExtra(Intent.EXTRA_TEXT, "Checkout this item on Etsy " + listing.title + " " + listing.url);
+                    intent.setType("text/plain");
+
+                    activity.startActivityForResult(Intent.createChooser(intent, "Share"), REQUEST_CODE_SHARE);
+                }
+            });
+        }
+
         Picasso.with(listingHolder.imageView.getContext())
                 .load(listing.Images[0].url_570xN)
                 .into(listingHolder.imageView);
@@ -72,12 +127,34 @@ public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingH
         return activeListings;
     }
 
+    @Override
+    public void onConnected() {
+        if(getItemCount() == 0){
+            Etsy.getActiveListings(this);
+        }
+        isPlayAvailable = true;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onDisconnected() {
+
+        if(getItemCount() == 0){
+            Etsy.getActiveListings(this);
+        }
+
+        isPlayAvailable = false;
+        notifyDataSetChanged();
+    }
+
     public class ListingHolder extends RecyclerView.ViewHolder{
 
         public ImageView imageView;
         public TextView titleView;
         public TextView shopNameView;
         public TextView priceView;
+        public PlusOneButton plusOneButton;
+        public ImageButton shareButton;
 
 
         public ListingHolder(View itemView) {
@@ -87,6 +164,8 @@ public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingH
             titleView = (TextView) itemView.findViewById(R.id.listing_title);
             shopNameView = (TextView) itemView.findViewById(R.id.listing_shop_name);
             priceView = (TextView) itemView.findViewById(R.id.listing_price);
+            plusOneButton = (PlusOneButton) itemView.findViewById(R.id.listing_plus_one_btn);
+            shareButton = (ImageButton) itemView.findViewById(R.id.listing_share_btn);
         }
     }
 }
